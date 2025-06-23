54493c314f3afc0b31c1f27edd38097c2a8be03f
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-23 12:53:07 +0100

    Updated Changes

--- dashboard/models.py
+++ dashboard/models.py
@@ -19,4 +19,6 @@ class Product(models.Model):
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
-    staff = models.ForeignKey(models.CASCADE, null=True)
+    staff = models.ForeignKey(models.CASCADE, null=True).
+    order_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto)
