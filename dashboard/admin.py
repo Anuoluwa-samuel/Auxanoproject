39dc5478387bda937673e128249058c591d97768
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-23 13:13:07 +0100

    Updated Changes

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Product
+from .models import Product
 from django.contrib.auth.models import Group
 
 admin.site.site_header = 'Timeless Dashboard'
@@ -9,7 +10,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['category',]
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
-admin.site.register(order, ProductAdmin)
+admin.site.register(order)
 
 
 # admin.site.unregister(Group)
