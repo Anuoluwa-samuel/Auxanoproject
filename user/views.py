315f626f1f070697fb5ab564cd11f986787df421
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-23 15:06:08 +0100

    Updated Changes

--- user/views.py
+++ user/views.py
@@ -6,7 +6,8 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save(),
+            form.save()
+            return redirect('')
     else:
         form = UserCreationForm()
     context = {
