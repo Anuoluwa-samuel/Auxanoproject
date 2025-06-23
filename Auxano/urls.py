5272e7492c7719c970dfe164c7c99b3df2111a8f
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-23 14:02:07 +0100

    Updated Changes

--- Auxano/urls.py
+++ Auxano/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from user 
+from user import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
