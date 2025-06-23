a7847c29a4ea8c4bd0f386037164c4c431dfec40
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-23 14:05:08 +0100

    Updated Changes

--- Auxano/urls.py
+++ Auxano/urls.py
@@ -21,5 +21,5 @@ from user import views as user_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
-    path{'register/', user_view.register, name= 'user-regos77'} 
+    path('register/', user_view.register, name= 'user-register')
 ]
