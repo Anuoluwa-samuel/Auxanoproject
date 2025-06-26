a963122852831eb6af4ea3593c40a86ff8b748cf
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-26 14:27:47 +0100

    feat: Implement ProductForm in dashboard

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,7 +1,7 @@
-# from django import forms
-# from .models import Product
+from django import forms
+from .models import Product
 
-# class ProductForm(forms.ModelForm):
-#     class Meta:
-#         model = Product
-#         fields = ['name', 'category', 'quantity']
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'category', 'quantity']
