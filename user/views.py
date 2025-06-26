8a16c6639797aa548603e612550afae14a0b7cce
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-26 10:51:59 +0100

    Fix: Add missing newline at end of user/views.py

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+
 # Create your views here.
 
 
