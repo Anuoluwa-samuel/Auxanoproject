8c07ed8383a36c26920658f0fbdb7c478034b178
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-26 10:11:58 +0100

    feat: Apply crispy forms styling to user profile update form

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
-from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
