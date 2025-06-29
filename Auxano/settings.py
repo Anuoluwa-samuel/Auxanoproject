5ed2613d7d0781df9b9254e4bc7dfa012583b4e5
Timeless <Ayomidesamuel365@gmail.com>
2025-06-29 19:22:52 +0100

    Fix: Configure SMTP email backend

--- Auxano/settings.py
+++ Auxano/settings.py
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
