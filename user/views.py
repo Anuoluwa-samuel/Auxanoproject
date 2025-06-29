6b8ce3a2c82dad14b68921095a06953edcc3b8e4
Timeless <Ayomidesamuel365@gmail.com>
2025-06-29 17:14:51 +0100

    Fix: Display success message after user registration

--- user/views.py
+++ user/views.py
@@ -12,7 +12,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request)
+            messages.success(request, f'')
             return redirect('user-login')
     else:
         form = CreateUserForm()
