2ae2799e106e7065ae7b382df417850321308574
Timeless <Ayomidesamuel365@gmail.com>
2025-06-29 19:56:53 +0100

    Fix: Add password reset complete template

--- Auxano/urls.py
+++ Auxano/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='dashboard/password_reset.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='dashboard/password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='dashboard/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='dashboard/password_reset_complete.html'), name='password_reset_complete'),
     
          
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
