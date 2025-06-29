f22db37815eef4756d59f988307f6df67dbb0db0
Timeless <Ayomidesamuel365@gmail.com>
2025-06-29 19:19:53 +0100

    feat: Use console email backend for development

--- Auxano/settings.py
+++ Auxano/settings.py
@@ -144,9 +144,9 @@ LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'Ayomidesamuel365@gmail.com'
-EMAIL_HOST_PASSWORD = 'nzgdhetpgwkhxisj'
+EMAIL_HOST_PASSWORD = 'nzgd hetp gwkh xisj'
