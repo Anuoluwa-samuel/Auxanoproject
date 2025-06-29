d989864ac030c0f03a67c4f74b25111512485297
Timeless <Ayomidesamuel365@gmail.com>
2025-06-29 19:53:53 +0100

    Fix: Specify template for password reset confirm view

--- Auxano/urls.py
+++ Auxano/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('profile/', user_view.profile, name= 'user-profile'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='dashboard/password_reset.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='dashboard/password_reset_done.html'), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='dashboard/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
          
