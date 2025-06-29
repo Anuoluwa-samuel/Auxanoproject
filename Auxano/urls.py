4d31112ea52bb3a3719f32790c92da62cc5b8b1d
Timeless <Ayomidesamuel365@gmail.com>
2025-06-29 18:42:52 +0100

    Fix: Correct password reset complete URL and add static media URL serving

--- Auxano/urls.py
+++ Auxano/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/, auth_views.PasswordResetCompleteView.as_view(), name='password_reset_confirm'),
     
          
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
