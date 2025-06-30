c31011a3907a377702affc2c8b79998bbcb0ae2a
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-06-30 10:23:25 +0100

    Fix: Remove quantity from Product string representation

--- dashboard/models.py
+++ dashboard/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
 
 
     def __str__(self):
-        return f'{self.name} - {self.quantity}'
+        return f'{self.name}'
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
