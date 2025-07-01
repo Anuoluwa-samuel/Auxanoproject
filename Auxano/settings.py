475a126984b97ba98470dda434a60ed5f98ea72a
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-01 08:31:38 +0100

    feat: Implement two-factor authentication and update profile image field

--- Auxano/settings.py
+++ Auxano/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django_otp',
     'django_otp.plugins.otp_totp',
     'two_factor',
-
+    'two_factor.plugins.phonenumber',
     'two_factor.plugins.email',
     'qrcode',
 ]
