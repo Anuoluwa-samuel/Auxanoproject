d71b50c089ec46110e4951399ef2b19a3c7a3a1b
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-01 08:14:38 +0100

    feat: Enable Django OTP middleware

--- Auxano/settings.py
+++ Auxano/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_otp.middleware.OTPMiddleware',
 ]
 
 ROOT_URLCONF = 'Auxano.urls'
