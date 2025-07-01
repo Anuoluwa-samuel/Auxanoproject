497734748ace2deda32e6b0833335a7489dfbccd
Ogunyemi Ayomide <ayomidesamuel365@gmail.com>
2025-07-01 08:36:39 +0100

    Fix: Complete URL patterns and add media serving

--- Auxano/urls.py
+++ Auxano/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='dashboard/password_reset_done.html'), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='dashboard/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='dashboard/password_reset_complete.html'), name='password_reset_complete'),
+    path('account/login/', auth_views.)
     
          
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
